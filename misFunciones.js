1168fed4ba179a18c7a82290e2d2937f29176a43
Npetrashin <80826531+Npetrashin@users.noreply.github.com>
2021-06-01 18:41:18 -0300

    agregados botones de redireccion

---
+++ misFunciones.js
@@ -0,0 +1,33 @@
+/**
+
+* Esta es una funcion para ingresar al juego en si
+* @method CargarWeb
+* @param N/A
+* @param N/A
+* @return N/A
+*/
+
+function CargarWeb(){
+    var URLcomp;
+
+    URLcomp= "Juego.html";
+    window.open(URLcomp)
+
+}
+
+/**
+
+ * Esta es una funcion para regresar a la pagina principal
+ * @method Regresar
+ * @param N/A
+ * @param N/A
+ * @return N/A
+ */
+
+
+function Regresar(){
+    var URLcomp;
+    URLcomp= "index.html";
+    window.open(URLcomp)
+
+}
